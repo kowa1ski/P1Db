160149acba8023fba6bddac14729759ee24c3070
kowa1ski <kowalski@gmx.es>
2017-08-21 11:29:32 +0200

    Creado Layout de agregación de registro

--- app/src/main/java/com/example/android/p1db/AgregarActivity.java
+++ app/src/main/java/com/example/android/p1db/AgregarActivity.java
@@ -6,13 +6,19 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class AgregarActivity extends AppCompatActivity {
+
+    EditText nombre, apellido, pass, time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar);
+
+
 
     }
 
