05fbb316ac53b9a489fd2bf2aff0fba808bdbc4e
kowa1ski <kowalski@gmx.es>
2017-08-21 11:40:18 +0200

    Probando funcionalidad del botón de agregar registro. CyF. Comprobado y funcionando

--- app/src/main/java/com/example/android/p1db/AgregarActivity.java
+++ app/src/main/java/com/example/android/p1db/AgregarActivity.java
@@ -6,20 +6,42 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class AgregarActivity extends AppCompatActivity {
+public class AgregarActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText nombre, apellido, pass, time;
+    Button buttonAgregarRegistro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar);
 
+        buttonAgregarRegistro = (Button) findViewById(R.id.buttonInsertReg);
+
+        buttonAgregarRegistro.setOnClickListener(this);
 
 
     }
 
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+
+            case R.id.buttonInsertReg:
+
+                Toast.makeText(this, "Funciona el jodido botón, ou yea!!", Toast.LENGTH_LONG).show();
+
+                break;
+            default:
+
+        }
+
+    }
 }
