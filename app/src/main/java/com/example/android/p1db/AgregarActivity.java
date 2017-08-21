4d1aa8952917ee6d5b692ee1307308820dda85ea
kowa1ski <kowalski@gmx.es>
2017-08-21 10:58:55 +0200

    creado el intent a AGREGAR

---
+++ app/src/main/java/com/example/android/p1db/AgregarActivity.java
@@ -0,0 +1,19 @@
+package com.example.android.p1db;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class AgregarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar);
+
+    }
+
+}
