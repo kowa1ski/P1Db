2d5f06153459d8baa8d38101b26ba5378b65c0c0
kowa1ski <kowalski@gmx.es>
2017-08-21 12:26:33 +0200

    extendemos de SQLiteOpeHelper y Creamos los métodos y el constructor con la bombilla

--- app/src/main/java/com/example/android/p1db/data/VigDbHelper.java
+++ app/src/main/java/com/example/android/p1db/data/VigDbHelper.java
@@ -1,8 +1,28 @@
 package com.example.android.p1db.data;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by Usuario on 21/08/2017.
  */
 
-public class VigDbHelper {
+                        // extendemos de SQLiteOpeHelper
+public class VigDbHelper extends SQLiteOpenHelper {
+
+    //Creamos los métodos y el constructor con la bombilla
+    public VigDbHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
